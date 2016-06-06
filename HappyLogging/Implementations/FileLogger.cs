eb03731e5d47e111c3f83953e8410f82d3a88666
ProductiveRage <742348+ProductiveRage@users.noreply.github.com>
2016-06-06 23:22:16 +0100

    Extended FileLogger to allow for the destination file's folder to be created (if it doesn't already) and for an optional lock object to be provided, for when the calling code may want to provide one

--- HappyLogging/Implementations/FileLogger.cs
+++ HappyLogging/Implementations/FileLogger.cs
@@ -8,33 +8,79 @@ namespace HappyLogging
 	/// </summary>
 	public sealed class FileLogger : TextLogger
 	{
-		public FileLogger(Func<FileInfo> fileRetriever, Func<LogEventDetails, string> messageFormatter, ErrorBehaviourOptions individualLogEntryErrorBehaviour)
-			: base(messageFormatter, GetOutputWriter(fileRetriever), individualLogEntryErrorBehaviour) { }
+		public FileLogger(
+			Func<FileInfo> fileRetriever,
+			Func<LogEventDetails, string> messageFormatter,
+			ErrorBehaviourOptions individualLogEntryErrorBehaviour,
+			bool tryToCreateFolderIfRequired,
+			object optionalSyncLock)
+			: base(messageFormatter, GetOutputWriter(null, fileRetriever, tryToCreateFolderIfRequired, optionalSyncLock), individualLogEntryErrorBehaviour) { }
 
-		public FileLogger(FileInfo file, Func<LogEventDetails, string> messageFormatter, ErrorBehaviourOptions individualLogEntryErrorBehaviour)
-			: this(() => file, messageFormatter, individualLogEntryErrorBehaviour)
+		public FileLogger(
+			FileInfo file,
+			Func<LogEventDetails, string> messageFormatter,
+			ErrorBehaviourOptions individualLogEntryErrorBehaviour,
+			bool tryToCreateFolderIfRequired,
+			object optionalSyncLock)
+			: base(messageFormatter, GetOutputWriter(file, null, tryToCreateFolderIfRequired, optionalSyncLock), individualLogEntryErrorBehaviour) { }
+
+		public FileLogger(Func<FileInfo> fileRetriever, bool tryToCreateFolderIfRequired, object optionalSyncLock)
+			: this(fileRetriever, Defaults.MessageFormatter, Defaults.IndividualLogEntryErrorBehaviour, tryToCreateFolderIfRequired, optionalSyncLock) { }
+
+		public FileLogger(Func<FileInfo> fileRetriever)
+			: this(fileRetriever, Defaults.MessageFormatter, Defaults.IndividualLogEntryErrorBehaviour, Defaults.TryToCreateFolderIfRequired, optionalSyncLock: null) { }
+
+		public FileLogger(FileInfo file)
+			: this(file, Defaults.MessageFormatter, Defaults.IndividualLogEntryErrorBehaviour, Defaults.TryToCreateFolderIfRequired, optionalSyncLock: null) { }
+
+		public new static class Defaults
 		{
-			if (file == null)
-				throw new ArgumentNullException(nameof(file));
+			public static Func<LogEventDetails, string> MessageFormatter { get { return TextLogger.Defaults.MessageFormatter; } }
+			public static ErrorBehaviourOptions IndividualLogEntryErrorBehaviour { get { return TextLogger.Defaults.IndividualLogEntryErrorBehaviour; } }
+			public static bool TryToCreateFolderIfRequired { get { return true; } }
 		}
-		
-		public FileLogger(Func<FileInfo> fileRetriever) : this(fileRetriever, Defaults.MessageFormatter, Defaults.IndividualLogEntryErrorBehaviour) { }
-		
-		public FileLogger(FileInfo file) : this(file, Defaults.MessageFormatter, Defaults.IndividualLogEntryErrorBehaviour) { }
 
-		private static Action<string> GetOutputWriter(Func<FileInfo> fileRetriever)
+		private static Action<string> GetOutputWriter(FileInfo file, Func<FileInfo> fileRetriever, bool tryToCreateFolderIfRequired, object optionalSyncLock)
 		{
-			if (fileRetriever == null)
-				throw new ArgumentNullException(nameof(fileRetriever));
+			if ((file == null) && (fileRetriever == null))
+				throw new ArgumentException($"One of {nameof(file)} and {nameof(fileRetriever)} must be non-null but they are both null");
+			if ((file != null) && (fileRetriever != null))
+				throw new ArgumentException($"Precisely one of {nameof(file)} and {nameof(fileRetriever)} must be non-null but they are both non-null");
 
 			return content =>
 			{
-				var file = fileRetriever();
-				if (file == null)
-					throw new Exception("fileRetriever returned null FileInfo reference");
-				using (var writer = file.AppendText())
+				FileInfo fileToWriteTo;
+				if (file != null)
+					fileToWriteTo = file;
+				else
 				{
-					writer.WriteLine(content);
+					fileToWriteTo = fileRetriever();
+					if (fileToWriteTo == null)
+						throw new Exception("fileRetriever returned null FileInfo reference");
+				}
+				if (tryToCreateFolderIfRequired)
+				{
+					var folder = fileToWriteTo.Directory;
+					folder.Refresh();
+					if (!folder.Exists)
+						folder.Create();
+				}
+				if (optionalSyncLock == null)
+				{
+					using (var writer = fileToWriteTo.AppendText())
+					{
+						writer.WriteLine(content);
+					}
+				}
+				else
+				{
+					lock (optionalSyncLock)
+					{
+						using (var writer = fileToWriteTo.AppendText())
+						{
+							writer.WriteLine(content);
+						}
+					}
 				}
 			};        
 		}
