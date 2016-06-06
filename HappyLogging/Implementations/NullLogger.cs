5bb7e38b356f15d30217eef85f65fe1ac10095d9
ProductiveRage <742348+ProductiveRage@users.noreply.github.com>
2016-06-06 22:34:26 +0100

    Make NullLogger's constructor private and share a single instance between all consumers, no point creating new ones each time - more work for the GC with no benefit

--- HappyLogging/Implementations/NullLogger.cs
+++ HappyLogging/Implementations/NullLogger.cs
@@ -10,6 +10,10 @@ namespace HappyLogging.Implementations
 	/// </summary>
 	public sealed class NullLogger : ILogEvents
 	{
+		private readonly static NullLogger _instance = new NullLogger();
+		public static NullLogger Instance = _instance;
+		private NullLogger() { }
+
 		/// <summary>
 		/// This should throw an exception for a null message set but whether exceptions are thrown due to any other issues (eg. a message whose ContentGenerator
 		/// delegate throws an exception or IO exceptions where file-writing is attempted) will vary depending upon the implementation
