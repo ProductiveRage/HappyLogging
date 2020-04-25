9b56af17674a14ef57d7cb32461877be9ae36b78
Dan Roberts <dangger36@gmail.com>
2020-04-25 20:28:32 +0100

    Add a "BuildFromNuGet" folder because I'd forgotten how to do this pre-new-format .csproj files!

---
+++ BuildNuGetPackage/BuildFromRelease.bat
@@ -0,0 +1,15 @@
+@echo off
+
+%~d0
+cd "%~p0"
+
+del *.nu* 2> nul
+del *.dll 2> nul
+del *.pdb 2> nul
+del *.xml 2> nul
+
+copy ..\HappyLogging\bin\Release\HappyLogging.dll > nul
+copy ..\HappyLogging\bin\Release\HappyLogging.xml > nul
+
+copy ..\HappyLogging.nuspec > nul
+nuget pack -NoPackageAnalysis HappyLogging.nuspec
