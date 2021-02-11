5dd010116a3c2b2b996e3683c738e3d037af4bb0
Dion Williams <dionrhys1@gmail.com>
2021-02-11 14:02:10 +0000

    Add .pdb file to NuGet package for better debugging

--- BuildNuGetPackage/BuildFromRelease.bat
+++ BuildNuGetPackage/BuildFromRelease.bat
@@ -9,6 +9,7 @@ del *.pdb 2> nul
 del *.xml 2> nul
 
 copy ..\HappyLogging\bin\Release\HappyLogging.dll > nul
+copy ..\HappyLogging\bin\Release\HappyLogging.pdb > nul
 copy ..\HappyLogging\bin\Release\HappyLogging.xml > nul
 
 copy ..\HappyLogging.nuspec > nul
